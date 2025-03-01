72bf55393a7e34e5603144101610cc3901f0d523
Genaro Gonzalez <genarrogg@gmail.com>
2025-03-01 15:33:40 -0400

    graphql

--- index.ts
+++ index.ts
@@ -1,22 +1,43 @@
 import Fastify, { FastifyInstance } from 'fastify'
+import mercurius from 'mercurius'
 
 const server: FastifyInstance = Fastify({})
 import { healthcheck } from "./src/routers/index"
 
+server.register(healthcheck, { prefix: '/' })
 
-server.register(healthcheck, { prefix: '/' });
+// Define tu esquema GraphQL
+const schema = `
+  type Query {
+    hello: String
+  }
+`
+
+// Define tus resolvers
+const resolvers = {
+  Query: {
+    hello: async () => 'Hello world!'
+  }
+}
+
+// Registra mercurius en el servidor Fastify
+server.register(mercurius, {
+  schema,
+  resolvers,
+  graphiql: true
+})
 
 const start = async () => {
-    try {
-        await server.listen({ port: 3000 })
+  try {
+    await server.listen({ port: 3000 })
 
-        const address = server.server.address()
-        const port = typeof address === 'string' ? address : address?.port
+    const address = server.server.address()
+    const port = typeof address === 'string' ? address : address?.port
 
-    } catch (err) {
-        server.log.error(err)
-        process.exit(1)
-    }
+  } catch (err) {
+    server.log.error(err)
+    process.exit(1)
+  }
 }
 
 start()
