18d64f11ee20f7e21a9b96cec582d4aeb9fcfdc8
Genaro Gonzalez <genarrogg@gmail.com>
2025-03-09 19:31:30 -0400

    empesando la integracion de apollo

--- src/pdf/endpoint.ts
+++ src/pdf/endpoint.ts
@@ -3,53 +3,48 @@ import * as templates from "./templates";
 import { createElement } from "react";
 
 const schema = `
+  input DataInput {
+    message: String
+    items: [String]
+  }
+
   type Query {
-    generatePDF(template: String!, data: String!): String
+    generatePDF(template: String!, data: DataInput!): String
   }
 `;
 
 const resolvers = {
-    Query: {
-        generatePDF: async (_: any, { template, data }: { template: string; data: string }) => {
-            try {
+  Query: {
+    generatePDF: async (_: any, { template, data }: { template: string; data: { message: string; items: string[] } }) => {
+      try {
+        const componentName = template
+          .split("-")
+          .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+          .join("") + "Template";
 
-                const componentName = template
-                    .split("-")
-                    .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
-                    .join("") + "Template";
+        const Template = templates[componentName as keyof typeof templates];
 
+        if (!Template) {
+          throw new Error("Template not found");
+        }
 
-                const Template = templates[componentName as keyof typeof templates];
+        // Asegurar que el tipo sea correcto
+        const MyDocument: any = createElement(Template, { data });
 
-                if (!Template) {
-                    throw new Error("Template not found");
-                }
+        // Renderizar a stream el documento correctamente tipado
+        const stream = await renderToStream(MyDocument);
 
-                let templateData;
-                try {
-
-                    templateData = { data: JSON.parse(data) };
-                } catch (error) {
-                    throw new Error("Invalid data format");
-                }
-
-                // Asegurar que el tipo sea correcto
-                const MyDocument: any = createElement(Template, templateData);
-
-                // Renderizar a stream el documento correctamente tipado
-                const stream = await renderToStream(MyDocument);
-
-                return new Promise((resolve, reject) => {
-                    const chunks: Buffer[] = [];
-                    stream.on("data", (chunk) => chunks.push(chunk));
-                    stream.on("end", () => resolve(Buffer.concat(chunks).toString("base64")));
-                    stream.on("error", (error) => reject(error));
-                });
-            } catch (error) {
-                throw new Error("Error generating PDF: " + (error instanceof Error ? error.message : "Unknown error"));
-            }
-        },
+        return new Promise((resolve, reject) => {
+          const chunks: Buffer[] = [];
+          stream.on("data", (chunk) => chunks.push(chunk));
+          stream.on("end", () => resolve(Buffer.concat(chunks).toString("base64")));
+          stream.on("error", (error) => reject(error));
+        });
+      } catch (error) {
+        throw new Error("Error generating PDF: " + (error instanceof Error ? error.message : "Unknown error"));
+      }
     },
+  },
 };
 
 export { schema, resolvers };
