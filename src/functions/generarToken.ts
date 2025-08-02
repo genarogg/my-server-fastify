444b8d28c4228f3382bacc145fc6635dce76d2ea
Genaro Gonzalez <gena_gonzalez15@hotmail.com>
2025-08-02 10:04:32 -0400

    fix

--- src/functions/generarToken.ts
+++ src/functions/generarToken.ts
@@ -1,29 +1,25 @@
 import jwt from "jsonwebtoken";
 
 interface Usuario {
-    id: number;
+  id: number;
 }
 
 const generarToken = (usuario: Usuario): string => {
-    const JWTSECRETO: any = process.env.JWTSECRETO || "jwt-secret";
-    const JWTTIEMPO: any = process.env.JWTTIEMPO || "1d";
+  const JWTSECRETO = process.env.JWTSECRETO ?? "jwt-secret";
+  const JWTTIEMPO = process.env.JWTTIEMPO ?? "1d";
 
-    const { id } = usuario;
+  const { id } = usuario;
 
-    try {
-        const token = jwt.sign(
-            { id },
-            JWTSECRETO,
-            {
-                algorithm: "HS256",
-                expiresIn: JWTTIEMPO,
-            }
-        );
+  try {
+    const token = jwt.sign({ id }, JWTSECRETO, {
+      algorithm: "HS256",
+      expiresIn: JWTTIEMPO,
+    });
 
-        return token;
-    } catch (error: any) {
-        throw new Error(`Error al generar el token: ${error.message}`);
-    }
+    return token;
+  } catch (error: any) {
+    throw new Error(`Error al generar el token: ${error.message}`);
+  }
 };
 
 export default generarToken;
