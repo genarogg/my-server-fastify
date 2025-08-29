af2b1526b3d904026aebc386daa6f3d94d4289fb
Genaro Gonzalez <gena_gonzalez15@hotmail.com>
2025-08-29 01:20:28 -0400

    pagina de inicio

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import App from './app/App'
+import App from './app/page'
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
